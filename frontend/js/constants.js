a9995f6af040c8108b0aadb135d8719628d9ce91
Krs Paints <krs@artdreamer.org>
2022-04-20 11:19:40 +0900

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xC22507BB49AfB196498F7B07b7CC7Eef7CEd1Bef"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
